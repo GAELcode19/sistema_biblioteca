9c4b876770603dc20723f29f314a30bbea0e9ba0
Ximena Alvarez Rocha <alvarezrochax@gmail.com>
2026-03-16 11:09:15 -0600

    modulo de calificaciones con estrellas

---
+++ app/Models/Article.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id'
+    ];
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
+
+    public function averageRating()
+    {
+        return $this->ratings()->avg('rating');
+    }
+}
